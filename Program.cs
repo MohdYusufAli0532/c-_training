d86e44150762316b905e6ea3849a930ead5df776
MohdYusufAli0532 <mohd0532ali@gmail.com>
2025-12-11 14:31:27 +0530

    Day1 Code

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿// See https://aka.ms/new-console-template for more information
+class first
+    {
+    public first()
+    {
+        System.Console.WriteLine("This is first class constructor");
+    }
+    public void one()
+    {
+               System.Console.WriteLine("This is first class method");
+
+    }
+    private void private_one()
+        {
+               System.Console.WriteLine("This is first class private method");
+        }
+
+    public void private_access()     {
+        private_one();
+    }
+}
+class second:first
+{
+    public second()
+    {
+        System.Console.WriteLine("This is second class constructor");
+    }
+    public void two()
+    {
+               System.Console.WriteLine("This is second class method");
+    }
+}
+
+class Logic
+{
+       public static void Main(string[] args)
+    {
+        Student obj=new Student();
+
+        obj.msg();
+        obj.show();
+
+        //System.Console.WriteLine("Name:"+obj.name);
+        //System.Console.WriteLine("Age:" + obj.age);
+        //System.Console.WriteLine("roll Number:" + obj.rollnum);
+        //System.Console.WriteLine("Course:" + obj.Course);
+        //System.Console.WriteLine("number:" + obj.number);
+
+
+        Student stu1=new Student("Ali",22,4,"CSE",4342);
+        stu1.show();
+
+        //System.Console.WriteLine("Name:" + stu1.name);
+        //System.Console.WriteLine("Age:" + stu1.age);
+        //System.Console.WriteLine("roll number:" + stu1.rollnum);
+        //System.Console.WriteLine("Course:" + stu1.Course);
+        //System.Console.WriteLine("Number:" + stu1.number);
+
+
+        Length obj1 = new Length(12345);
+        Length obj2 = new Length("Hello, World!");
+
+
+        second objectt = new second();
+        objectt.one();
+        objectt.two();
+        objectt.private_access();
+
+
+    }
+        
+}
---
+++ Student.cs
@@ -0,0 +1,60 @@
+﻿using System;
+class Student
+{
+    public String name;
+    public int age;
+    public int rollnum;
+    public String Course;
+    public int number;
+    public Student()
+    {
+        name = "Yusuf";
+        age = 21;
+        rollnum = 6;
+        Course = "CSE";
+        number = 12345;
+    }
+    public Student(String name, int age, int rollnum, String Course, int number)
+    {
+        this.name = name;
+        this.age = age;
+        this.rollnum = rollnum;
+        this.Course = Course;
+        this.number = number;
+    }
+    public void show()
+    {
+
+        System.Console.WriteLine("Name:" + name);
+        System.Console.WriteLine("Age:" + age);
+        System.Console.WriteLine("RollNum:" + rollnum);
+        System.Console.WriteLine("Course:" + Course);
+        System.Console.WriteLine("numbers:" + number);
+    }
+
+    public void msg()
+    {
+        System.Console.WriteLine("Hello I am a student");
+
+    }
+
+}
+
+public class Length
+{
+    public Length(int num)
+    {
+        int count = 0;
+        while (num != 0)
+        {
+            num /= 10;
+            count++;
+        }
+        Console.WriteLine("Number of digits:"+count);
+    }
+
+    public Length(string text)
+    {
+        Console.WriteLine("Number of characters:"+text.Length);
+    }
+}
